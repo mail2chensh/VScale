e960027d96a0498d7bd5468bc7ef2251db5aae0d
chensh <mail2chensh@gmail.com>
2017-02-16 14:08:20 +0800

    Initial Commit

---
+++ VScale/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VScale
+//
+//  Created by dev on 2017/2/16.
+//  Copyright © 2017年 Chensh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
